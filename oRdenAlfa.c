9daf251a8ae244cbec6f3c2069ab0c9c66ab7a31
William Elí O.L <ramun346416@gmail.com>
2022-12-27 07:38:28 +0000

    Avance
    Comienza a ordenar,pero le faltan mas bueltas de ciclo,al parecer

--- oRdenAlfa.c
+++ oRdenAlfa.c
@@ -10,6 +10,10 @@ void main()
     // Esto si hace el cambio, falta un vector intermedio
     //      for (int i = 0; i < MAXSIZE; i++)
     //    vector1 [i] =   vector [0][i] ;
+    for (int i = 0; i < MAXSIZE1; i++)
+    {
+        printf("\t%s\t", vector[i]);
+    }
 
     // Inicia prueba
 
@@ -25,7 +29,7 @@ void main()
 
                 // vector [j] = vector [j+1];
                 for (int k = 0; k < MAXSIZE; k++)
-                    vector1[k] = vector[j + 1][k];
+                    vector [j][k] = vector [j + 1][k];
 
                 // vector [j+1] = acum;
                 for (int k = 0; k < MAXSIZE; k++)
@@ -34,6 +38,16 @@ void main()
         }
     }
 
+    for (int i = 0; i < MAXSIZE1; i++)
+    {
+        printf("\t%s\t", vector[i]);
+    }
+    // printf("\n\t");
+    //  for (int i = 0; i < MAXSIZE; i++)
+    //   {
+    //     printf("%c", vector1 [i]);
+    //   }
+}
     for (int i = 0; i < MAXSIZE1; i++)
     {
         printf("\t%s\t", vector[i]);
