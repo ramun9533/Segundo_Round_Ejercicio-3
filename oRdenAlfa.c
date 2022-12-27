6aff2d6f9b9f647a50282c33de20f757dd49b5cf
William Elí O.L <ramun346416@gmail.com>
2022-12-27 06:02:02 +0000

    Se agrega el acomulador llamado vector1

--- oRdenAlfa.c
+++ oRdenAlfa.c
@@ -6,9 +6,10 @@ void main()
 {
 
     char vector [][MAXSIZE] = {"Brenda", "Daniel", "Gerardo", "Erika", "Fabian", "Carlos", "Alejandra"};
+    char vector1 [MAXSIZE];
     // Esto si hace el cambio, falta un vector intermedio
      for (int i = 0; i < MAXSIZE; i++)
-    vector [0][i] = vector [1][i]; 
+   vector1 [i] =   vector [0][i] ; 
     // char vector1 [MAXSIZE1][MAXSIZE] ;
     // int acum, acum1;
     // acum =6;
@@ -19,6 +20,11 @@ void main()
           {
             printf("\t%s\t", vector[i]);
          }
-         
+        //  printf("\n%s\t", vector1 [MAXSIZE]);
+        printf("\n\t");
+         for (int i = 0; i < MAXSIZE; i++)
+          {
+            printf("%c", vector1 [i]);
+         }
    
 }
