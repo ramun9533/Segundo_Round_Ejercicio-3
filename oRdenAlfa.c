2933055827201762ba594b1cb75e164383ce0464
William Elí O.L <ramun346416@gmail.com>
2022-12-27 07:32:18 +0000

    Update oRdenAlfa.c

--- oRdenAlfa.c
+++ oRdenAlfa.c
@@ -1,30 +1,46 @@
-#include<stdio.h>
+#include <stdio.h>
 
 #define MAXSIZE 20
 #define MAXSIZE1 8
 void main()
 {
 
-    char vector [][MAXSIZE] = {"Brenda", "Daniel", "Gerardo", "Erika", "Fabian", "Carlos", "Alejandra"};
-    char vector1 [MAXSIZE];
+    char vector[][MAXSIZE] = {"Brenda", "Daniel", "Gerardo", "Erika", "Fabian", "Carlos", "Alejandra"};
+    char vector1[MAXSIZE];
     // Esto si hace el cambio, falta un vector intermedio
-     for (int i = 0; i < MAXSIZE; i++)
-   vector1 [i] =   vector [0][i] ; 
-    // char vector1 [MAXSIZE1][MAXSIZE] ;
-    // int acum, acum1;
-    // acum =6;
-    // printf("\n%s\n", vector [acum]);
-    // if(vector [1][0] < vector [2][0])
-    // printf("\n%s\n", vector[2]);
-     for (int i = 0; i < MAXSIZE1; i++)
-          {
-            printf("\t%s\t", vector[i]);
-         }
-        //  printf("\n%s\t", vector1 [MAXSIZE]);
-        printf("\n\t");
-         for (int i = 0; i < MAXSIZE; i++)
-          {
-            printf("%c", vector1 [i]);
-         }
-   
+    //      for (int i = 0; i < MAXSIZE; i++)
+    //    vector1 [i] =   vector [0][i] ;
+
+    // Inicia prueba
+
+    for (int i = 0; i < MAXSIZE1 - 1; i++)
+    {
+        for (int j = 0; j < MAXSIZE1 - 1 - i; j++) // Pner atencion a este detalle para optimizatr el metodo
+        {
+            if (vector[j][MAXSIZE] > vector[j + 1][MAXSIZE])
+            {
+                // acum = vector [j];
+                for (int k = 0; k < MAXSIZE; k++)
+                    vector1[k] = vector[j][k];
+
+                // vector [j] = vector [j+1];
+                for (int k = 0; k < MAXSIZE; k++)
+                    vector1[k] = vector[j + 1][k];
+
+                // vector [j+1] = acum;
+                for (int k = 0; k < MAXSIZE; k++)
+                    vector[j + 1][k] = vector1[k];
+            }
+        }
+    }
+
+    for (int i = 0; i < MAXSIZE1; i++)
+    {
+        printf("\t%s\t", vector[i]);
+    }
+    // printf("\n\t");
+    //  for (int i = 0; i < MAXSIZE; i++)
+    //   {
+    //     printf("%c", vector1 [i]);
+    //   }
 }
